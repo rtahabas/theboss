0419abe887f5960eecb86c5b3da3384d0e1c922e
rtahabas <rtahabas@gmail.com>
2023-03-19 15:03:40 +0300

    [UPDATE]: Form Error Handling

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -27,13 +27,32 @@ function Create() {
   const [details, setDetails] = React.useState("");
   const [dueDetail, setDuedetail] = React.useState("");
   const [category, setCategory] = React.useState("");
+  const [users, setUsers] = React.useState("");
   const [assignedUser, setAssignedUser] = React.useState([]);
+  const [formError, setFormError] = React.useState(null);
 
-  console.log(document);
+  React.useEffect(() => {
+    if (document) {
+      const options = document.map((user) => {
+        return { value: user, label: user.displayName };
+      });
+      setUsers(options);
+    }
+  }, [document]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name, details, dueDetail, category.value);
+    setFormError(null);
+
+    if (!category) {
+      setFormError("Please select a category");
+      return;
+    }
+
+    if (assignedUser.length < 1) {
+      setFormError("Please assign the project to at least 1 user ");
+      return;
+    }
   };
 
   return (
@@ -58,8 +77,11 @@ function Create() {
         </label>
         <label>
           <span>Assign to :</span>
+          <Select options={users} onChange={(option) => setAssignedUser(option)} isMulti />
         </label>
         <Button type="submit">Add Project</Button>
+
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
