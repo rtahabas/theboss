e6ee72b73530a8d962070a9a3b644280f6c9d93a
rtahabas <rtahabas@gmail.com>
2023-04-12 03:25:21 +0300

    [FEAT]: Project Filter

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,15 +1,49 @@
 import { useCollection } from "../../hooks/useCollection";
-import { Container, Row, Col, Spacer } from "@nextui-org/react";
-import "./Dashboard.css";
+import { Container, Row, Spacer } from "@nextui-org/react";
 import ProjectList from "../components/ProjectList/ProjectList";
+import ProjectFilter from "./ProjectFilter";
+import { useState } from "react";
+import { useAuthContext } from "hooks/useAuthContext";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   const { document, error } = useCollection("projects");
+  const [filter, setFilter] = useState("All");
+  const { user } = useAuthContext();
+
+  const projectsFilter = (filter) => {
+    setFilter(filter);
+  };
+
+  const projects = document
+    ? document.filter((project) => {
+        switch (filter) {
+          case "All":
+            return true;
+          case "Mine":
+            let assignedToMe = false;
+            project.assignedUserList.filter((member) => {
+              if (member.id === user.uid) {
+                return (assignedToMe = true);
+              }
+            });
+            return assignedToMe;
+          case "Development":
+          case "Design":
+          case "Marketing":
+          case "Sales":
+            return project.category === filter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <Container gap={0}>
+      <ProjectFilter projectsFilter={projectsFilter} filter={filter} />
       <Spacer y={1} />
-      <Row gap={1}>{document && <ProjectList project={document} />}</Row>
+      <Row gap={1}>{document && <ProjectList project={projects} />}</Row>
       {error && <div>{error}</div>}
     </Container>
   );
---
+++ src/pages/dashboard/ProjectFilter.jsx
@@ -0,0 +1,29 @@
+import { Navbar } from "@nextui-org/react";
+
+import "./ProjectFilter.css";
+
+const filterList = ["All", "Mine", "Development", "Design", "Marketing", "Sales"];
+
+const ProjectFilter = ({ filter, projectsFilter }) => {
+  return (
+    <div className="project-filter">
+      <Navbar isBordered variant="sticky">
+        <Navbar.Content hideIn="xs">
+          {filterList.map((filterList) => {
+            return (
+              <Navbar.Link
+                onPress={() => projectsFilter(filterList)}
+                isActive={filter === filterList ? true : false}
+                key={filterList}
+              >
+                {filterList}
+              </Navbar.Link>
+            );
+          })}
+        </Navbar.Content>
+      </Navbar>
+    </div>
+  );
+};
+
+export default ProjectFilter;
